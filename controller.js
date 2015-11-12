cffed0f374c4a40134ff17e57a0b2d230bc6f5ec
chancer4 <chance.rhodes5@gmail.com>
2015-11-12 16:08:25 -0500

    mapsAG
    added info boxes and the ability to get directions

--- controller.js
+++ controller.js
@@ -1,18 +1,87 @@
 angular.module('myApp',[]).controller('mapCtrl', function($scope){
 	
 	var mapOptions = {
-		zoom: 4;
+		zoom: 4,
 
 		//center of the us
-		center: new google.maps.LatLng(40.000,-98.0000),
+		center: new google.maps.LatLng(40.000,-98.0000)
 		// mapTypeId: google.maps.MaptTypeId.TERRAIN
 	}
 
 
-	$scope.map = new google.maps.Map($('#map'), mapOptions);
+	$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 	$scope.markers = [];
 
 	var infoWindow = new google.maps.InfoWindow();
+
+	var createMarker = function(city, index){
+		// console.log(city);
+		// console.log(index);
+
+		var latLon = city.latLon.split(',');
+		var lat = latLon[0];
+		var lon = latLon[1];
+		console.log(lat);
+		console.log(lon);
+		var marker = new google.maps.Marker({
+			map: $scope.map,
+			position: new google.maps.LatLng(lat, lon),
+			title: city.city,
+			icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7CFE7569'
+		})
+
+		markerContentHTML = '<div class="infoWindowContent">';
+	    	markerContentHTML += '<div class="total-pop">Total Population: ' + city.yearEstimate + '</div>';
+	    	markerContentHTML += '<div class="pop-dens-last-year">2010 Census: ' + city.lastCensus + '</div>';
+	    	markerContentHTML += '<div class="pop-change">Population Change %: ' + city.change + '</div>';
+	    	markerContentHTML += '<div class="pop-dens">Population Density: ' + city.lastPopDensity + '</div>';
+	    	markerContentHTML += '<div class="state">State: ' + city.state + '</div>';
+	    	markerContentHTML += '<div class="land-area">Land Area: ' + city.landArea + '</div>';
+	    	markerContentHTML += '<a href="#" onclick="getDirections('+lat+','+lon+')">Get directions</a>';
+    	markerContentHTML += '</div>';
+
+    	marker.content = markerContentHTML;
+    	google.maps.event.addListener(marker, 'click', function(){
+    		infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+    		infoWindow.open($scope.map, marker);
+    	});
+    	$scope.markers.push(marker);
+	}
+
+	$scope.triggerClick = function(i){
+		google.maps.event.trigger($scope.markers[i-1],"click")
+	}
+
+	getDirections = function(lat, lon){
+		var directionsService = new google.maps.DirectionsService();
+   		var directionsDisplay = new google.maps.DirectionsRenderer();
+   		var map = new google.maps.Map(document.getElementById('map'),{
+   			zoom: 7,
+   			mapTypeId: google.maps.MapTypeId.ROADMAP
+   		});
+   		directionsDisplay.setMap(map);
+   		directionsDisplay.setPanel(document.getElementById('list-window'));
+		
+		var request = {
+           //Origin hardcoded to Atlanta. Require geocode current loc,
+           //or give user input
+          origin: 'Atlanta, GA', 
+          destination:new google.maps.LatLng(lat,lon), 
+          travelMode: google.maps.DirectionsTravelMode.DRIVING
+        };
+
+        directionsService.route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+          }
+        });
+
+	}
+
+	$scope.cities = cities;
+	for(i=0;i<cities.length;i++){
+		createMarker(cities[i],i)
+	}
 
 
 });
