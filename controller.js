182fefac1c21ca81cfda92091913784eb86738e8
chancer4 <chance.rhodes5@gmail.com>
2015-11-12 14:13:20 -0500

    Added AngJS
    Added JS to pull up a map

--- controller.js
+++ controller.js
@@ -1,12 +1,18 @@
 angular.module('myApp',[]).controller('mapCtrl', function($scope){
 	
-	var mapOption = {
+	var mapOptions = {
 		zoom: 4;
 
 		//center of the us
 		center: new google.maps.LatLng(40.000,-98.0000),
-		mapTypeId: google.maps.MaptTypeId.TERRAIN
+		// mapTypeId: google.maps.MaptTypeId.TERRAIN
 	}
+
+
+	$scope.map = new google.maps.Map($('#map'), mapOptions);
+	$scope.markers = [];
+
+	var infoWindow = new google.maps.InfoWindow();
 
 
 });
