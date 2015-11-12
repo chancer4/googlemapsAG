bad2fffd4632c7452b38e2499df49d9fe9c151e8
chancer4 <chance.rhodes5@gmail.com>
2015-11-12 13:56:06 -0500

    New Project Started
    Using Sass, Angular, and Google Api

---
+++ controller.js
@@ -0,0 +1,12 @@
+angular.module('myApp',[]).controller('mapCtrl', function($scope){
+	
+	var mapOption = {
+		zoom: 4;
+
+		//center of the us
+		center: new google.maps.LatLng(40.000,-98.0000),
+		mapTypeId: google.maps.MaptTypeId.TERRAIN
+	}
+
+
+});
